1fc7283f632c924543635a6fe1a50d5b03e297fe
choisooyeonnie <liapie.taurus@gmail.com>
2024-02-06 19:12:57 +0400

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,145 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Girl:
+    def __init__(self):
+        self.name = "Jennie"
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def AboutMe(self):
+        print("User1: Hi there! My name is " + self.name + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.name = "Lily-Rose Depp"
+
+    def AboutMe(self):
+        print("User2: Hey! Nice to meet you~ And I'm " + self.name + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
+
+
+class Girl:
+    def __init__(self):
+        self.age = 19
+
+    def AboutMe(self):
+        print("User1: Nice to meet u, too!~ Btw, I'm " + self.age.__str__() + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.age = 15
+
+    def AboutMe(self):
+        print("User2: Omg, now I actually think that I'm too young >.< Ahahah, I'm " + self.age.__str__() + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
+
+
+class Girl:
+    def __init__(self):
+        self.height = 173
+
+    def AboutMe(self):
+        print("User1: Wow.. but it's okay! Oh, and as I can see from your bio, you're actually taller than me because I'm only " + self.height.__str__() + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.height = 176
+
+    def AboutMe(self):
+        print("User2: I'm dying over here, ahahaah~ Yea, I'm " + self.height.__str__() + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
+
+
+class Girl:
+    def __init__(self):
+        self.university = "Harvard University"
+
+    def AboutMe(self):
+        print("User1: Mmm, I see.. Actually, I'm currently studying at " + self.university.__str__() + ". What about you?" + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.school = 23
+
+    def AboutMe(self):
+        print("User2: FR?! That's my dream university T.T And I'm studying at school number " + self.school.__str__() + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
+
+
+class Girl:
+    def __init__(self):
+        self.career = "k-pop trainee"
+
+    def AboutMe(self):
+        print("User1: I'm sure you'll make it through and will achieve all your dreams! Now, as I said I'm a uni student, but i'm also a " + self.career.__str__() + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.career = "work"
+
+    def AboutMe(self):
+        print("User2: AOAOAOOAOAOAOOAOAO! I'M SERIOUSLY PLAYIN SIMS WITH A K=POP TRAINEE... omg, i'm literally in utter shock, ahaha~ As I'm still at school, I don't " + self.career.__str__() + ". I'm still shocked, okay?" + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
+
+
+class Girl:
+    def __init__(self):
+        self.food = "carbonara ramen"
+
+    def AboutMe(self):
+        print("User1: Ahahahahha, don't worry about it! Hmmm, do you have any favorite meal or snack? For instance, my favorite meal is " + self.food.__str__() + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.food = "tteokbokki"
+
+    def AboutMe(self):
+        print("User2: Now I'm hungry.. Oh, so my favorite food is " + self.food.__str__() + "I just can't live without it" + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
+
+
+class Girl:
+    def __init__(self):
+        self.drink = "strawberry lemonade"
+
+    def AboutMe(self):
+        print("User1: I think that both of these drinks can go with " + self.drink.__str__() + '\n')
+
+perfectsims = Girl()
+perfectsims.AboutMe()
+
+class Sims:
+    def __init__(self):
+        self.drink = "coca cola"
+
+    def AboutMe(self):
+        print("User2: Yea, kind of agree.. But my favorite is " + self.drink.__str__() + ". My one and only love, ahahaha" + '\n')
+
+perfectsims = Sims()
+perfectsims.AboutMe()
